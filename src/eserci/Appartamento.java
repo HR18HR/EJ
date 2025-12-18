b4ffc95f93d62e43f4364aedd6169abf4c518827
HR18HR <hamza.rofi@gmail.com>
2025-12-18 16:00:20 +0100

    esercizi

---
+++ src/eserci/Appartamento.java
@@ -0,0 +1,32 @@
+package eserci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Appartamento {
+    private float valore;
+    List<Stanza> appartamenti;
+    public Appartamento(List<Stanza> appartamenti, float valore) {
+        this.appartamenti = appartamenti;
+        this.valore = valore;
+    }
+
+    public float getValore() {
+        return valore;
+    }
+    public float Superfice(){
+        float superfice = 0;
+        for(Stanza s : appartamenti){
+            superfice+=s.Superfice();
+        }
+        return superfice;
+    }
+    public float getVolume(){
+        float volume = 0;
+        for(Stanza s : appartamenti){
+            volume+=s.Volume();
+        }
+        return volume;
+    }
+
+}
---
+++ src/eserci/MercatoImmobiliare.java
@@ -0,0 +1,19 @@
+package eserci;
+
+import java.util.List;
+
+public class MercatoImmobiliare {
+    List<Appartamento> appartamenti;
+
+    public void AddAppartamento(Appartamento appartamento){
+        appartamenti.add(appartamento);
+    }
+    public float getValore(Appartamento appartamento){
+        float valoremedio=0;
+        for(Appartamento ap:appartamenti){
+            valoremedio+=ap.getValore();
+        }
+        return (valoremedio/appartamenti.size())*appartamento.getValore();
+
+    }
+}
