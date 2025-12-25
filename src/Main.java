db692e9263cca78ad8f016fb3fe91223123cbb88
HR18HR <hamza.rofi@gmail.com>
2025-12-25 09:46:31 +0100

    esercizi_1_9

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) {
+
     }
 }
--- src/Match/SoccerMatch.java
+++ src/Match/SoccerMatch.java
@@ -3,7 +3,7 @@ package Match;
 public class SoccerMatch extends Match{
     int Gsq1,Gsq2;
     public SoccerMatch(String Sq1,String Sq2,int Gsq1,int Gsq2){
-        if(Sq1==null||Sq2==null)throw new RuntimeException();
+        super(Sq1,Sq2);
         this.Sq1=Sq1;
         this.Sq2=Sq2;
         this.Gsq1=Gsq1;
