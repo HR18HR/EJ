83f81efa7219b4e3cc8661e24ba20cf758e9d4c3
HR18HR <hamza.rofi@gmail.com>
2026-01-03 17:34:01 +0100

    niente

--- src/Libro/Libro.java
+++ src/Libro/Libro.java
@@ -14,4 +14,5 @@ public final class Libro {
     public boolean isDisponibile(){
         return Copie>0;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import Nodo.*;
 
 class Main  {
-
-
+    static Main k=new Main();
     public static void main(String[] args) {
+        System.out.println(k.hashCode());
 
     }
 
